7465db33f436670b14d92ba8ea98fd48472f011d
Тимофеев Даниил Васильевич <timofeev742@gmai.com>
2022-06-06 10:06:25 +0300

    Исправил модальное окно Ссылки

---
+++ RevolutionData/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevolutionData.Models
+{
+	/// <summary>
+	/// Заказ.
+	/// </summary>
+	internal class Order
+	{
+		/// <summary>
+		/// Id заказа.
+		/// </summary>
+		public int Id { get; set; }
+	}
+}
