267beb99487193250b109c88cff0afb133167cb8
Тимофеев Даниил Васильевич <timofeev742@gmai.com>
2022-06-14 21:22:30 +0300

    Добавил Identity

---
+++ RevolutionShopWebApp/Models/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RevolutionShopWebApp.Models.Entities
+{
+	public class User : IdentityUser
+	{
+	}
+}
--- RevolutionShopWebApp/Program.cs
+++ RevolutionShopWebApp/Program.cs
@@ -1,12 +1,25 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using RevolutionData.Interfaces;
 using RevolutionData.Models;
 using RevolutionData.Models.DB;
 using RevolutionData.Models.Entities;
 using RevolutionData.Models.Repositories;
+using RevolutionShopWebApp.Models.Entities;
+using RevolutionShopWebApp.Models.DB;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AppContextConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));;
+
+builder.Services.AddIdentity<User, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AppDbContext>();
+
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+var identityConnection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<RevolutionShopDbContext>(options => options.UseSqlServer(connection));
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -14,10 +27,11 @@ builder.Services.AddMvc();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.UseStaticFiles();
 
 app.Run();
