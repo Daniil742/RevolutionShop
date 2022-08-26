8f2feb1eeb3ec9483b9698b6fcaaaa9a53df41aa
Тимофеев Даниил Васильевич <timofeev742@gmai.com>
2022-08-26 12:29:28 +0300

    Добавил верстку и стили на большинство страниц

---
+++ RevolutionShopWebApp/wwwroot/js/Script.js
@@ -0,0 +1,15 @@
+﻿var modal = document.getElementById('fullscreen-menu-modal');
+var lin = document.getElementById('fullscreen-menu-link');
+var close = document.getElementsByClassName('close-fullscreen-menu')[0];
+
+lin.onclick = function () {
+    modal.style.display = "block";
+};
+close.onclick = function () {
+    modal.style.display = "none";
+};
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
